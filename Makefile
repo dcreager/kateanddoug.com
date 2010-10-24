ac2c2b693d9326e1b8f1980d7752f0d4a09a6b6a
Douglas Creager <douglas.creager@redjack.com>
2010-10-24 11:48:13 -0400

    Switching to nanoc for site generation
    I'm now using nanoc instead of Jekyll to generate the static content
    of the site.

--- Makefile
+++
@@ -1,6 +0,0 @@
-all:
-	compass compile
-	jekyll
-
-server:
-	jekyll --server
---
+++ config/compass.rb
@@ -0,0 +1,9 @@
+http_path    = "/"
+project_path = "."
+css_dir      = "output/css"
+sass_dir     = "content/css"
+images_dir   = "output/images"
+
+sass_options = {
+  :syntax => :scss
+}
